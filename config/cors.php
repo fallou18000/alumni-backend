e4bc5cd5cca5980ac4aa7f21a0606291581ea0aa
fallou18000 <serignefallouthioune260@gmail.com>
2026-05-12 00:05:34 +0000

    message

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173','http://localhost:5174', 'http://127.0.0.1:5174'],
+   'allowed_origins' => [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://alumni-frontend-khaki.vercel.app'
+],
 
     'allowed_origins_patterns' => [],
 
